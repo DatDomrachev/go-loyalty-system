a5257ca4ce9adf4e64d57cba9ebbe646d87bfd07
Dmitry Domrachev <D.Domrachev@x5.ru>
2022-02-15 23:39:34 +0300

    migrations for nothing

--- migrations/20211229144429_init_schema.sql
+++ migrations/20211229144429_init_schema.sql
@@ -1,14 +1,35 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE TABLE if not exists url (
+CREATE TABLE if not exists users (
 	id BIGSERIAL primary key,
-	full_url text,
+	login text,
+	password text,
 	user_token text,
+	balance float default 0.0,
+	withdrawn float default 0.0
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS unique_login_constrain ON users(login);
+CREATE UNIQUE INDEX IF NOT EXISTS unique_token_constrain ON users(user_token);
+
+CREATE TABLE if not exists transactions (
+	id BIGSERIAL primary key,
+	user_token text,
+	order_id text,
+	type integer,
+	status integer,
+	points float default 0.0,
+	uploaded_at TIMESTAMPTZ default now(),
+	processed_at TIMESTAMPTZ,
+	FOREIGN KEY (user_token) REFERENCES users (user_token)
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE if exists url;
+DROP TABLE if exists transactions;
+DROP INDEX IF exists unique_login_constrain;
+DROP INDEX IF exists unique_token_constrain;
+DROP TABLE if exists users;
 -- +goose StatementEnd
