2b327e99d888343aa05e732a91734aafc4ce7359
Dmitry Domrachev <D.Domrachev@x5.ru>
2022-02-16 05:34:45 +0300

    fix order

--- internal/app/repository/repository.go
+++ internal/app/repository/repository.go
@@ -71,7 +71,7 @@ type ProcessedWithdraw struct {
 }
 
 type ProcessingOrder struct {
-	OrderID    string  `json:"number"`
+	OrderID    string  `json:"order"`
 	Status     string  `json:"status"`
 	Accrual    float64 `json:"accrual,omitempty" default:"0.0"`
 }
